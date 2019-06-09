bfa88f50f83c933d287432527aea4a53db48fa12
Matvey <MostoMatvey@gmail.com>
2019-06-09 14:43:42 +0600

    Доработки

--- js/script.js
+++ js/script.js
@@ -1,7 +1,72 @@
 var btn = document.querySelector(".button-modal-window");
   
-var form = document.querySelector(".modal-window-wrap");
+var modalWindow = document.querySelector(".modal-window-wrap");
+
+var dateArrivel = modalWindow.querySelector("[name=arrivel-date]");
+var dateDeparture = modalWindow.querySelector("[name=date-of-departure]");
+var adults = modalWindow.querySelector("[name=amount-of-adults]");
+var children = modalWindow.querySelector("[name=amount-of-children]");
+
+var isStorageSupport = true;
+var storage = "";
+
+var plusAdults = modalWindow.querySelector("[name=button-plus-adults]");
+var minusAdults = modalWindow.querySelector("[name=button-minus-adults]");;
+var plusChildren = modalWindow.querySelector("[name=button-plus-children]");;
+var minusChildren = modalWindow.querySelector("[name=button-minus-children]"); 
+
+if (storage) {
+	dateArrivel.value = storage;
+	dateDeparture.value = storage;
+	adults.value = storage;
+	children.value = storage;
+}
+
+try{
+	storage = localStorage.getItem("dateArrivel");
+	storage = localStorage.getItem("dateDeparture");
+	storage = localStorage.getItem("adults");
+	storage = localStorage.getItem("children");
+} catch (err) {
+	isStorageSupport = false;
+}
+modalWindow.addEventListener("submit", function(evt){
+	if (!dateArrivel.value || !dateArrivel.value || !adults.value || !children.value) {
+		evt.preventDefault();
+		modalWindow.classList.remove("error");
+		modalWindow.offsetWidth = modalWindow.offsetWidth;
+		modalWindow.classList.add("error");
+	} else {
+		if (isStorageSupport) {
+			localStorage.setItem("dateArrivel", dateArrivel.value);
+			localStorage.setItem("dateDeparture", dateDeparture.value);
+			localStorage.setItem("adults", adults.value);
+			localStorage.setItem("children", children.value);
+		}
+	}
+})
 
 btn.addEventListener("click", function(evt){
-	form.classList.toggle("hide-form");
+	modalWindow.classList.toggle("hide-form");
 });
+
+
+plusAdults.addEventListener("click", function(evt){
+	adults.value++;
+})
+
+minusAdults.addEventListener("click", function(evt){
+	if (adults.value > 0) {
+		adults.value--;
+	}
+})
+
+plusChildren.addEventListener("click", function(evt){
+	children.value++;
+})
+
+minusChildren.addEventListener("click", function(evt){
+	if (children.value > 0) {
+		children.value--;
+	}
+})
